c02af2d8c2497b02f0b0070f0f1b3f014c13223c
PRICETRAVEL\carlos.cortez <carlos.cortez@pricetravel.com>
2019-05-27 19:35:31 -0500

    initial commit

---
+++ AppSyncConnect/Program.cs
@@ -0,0 +1,34 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace AppSyncConnect
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+
+         //var xclient = new HttpClientHandler();
+         //xclient.Properties.Add("x-api-key", "da2-bm2qqpx5ljhyhdqhv6nzsps7eu");
+
+         //xclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("x-api-key", "da2-bm2qqpx5ljhyhdqhv6nzsps7eu");
+
+
+         var client = new GraphQL.Client.GraphQLClient(new GraphQL.Client.GraphQLClientOptions()
+         {
+            EndPoint = new Uri("https://fo77ruxnqrh5tkml5oivdm4tvm.appsync-api.us-east-1.amazonaws.com/graphql"),
+         });
+
+         client.DefaultRequestHeaders.Add("x-api-key", "da2-bm2qqpx5ljhyhdqhv6nzsps7eu");
+
+         var response = client.GetQueryAsync("{\"query\" : \" { allCruisesQuote { provider} }\"}").Result;
+         Console.WriteLine(response);
+
+
+      }
+   }
+}
